42b741bb67bdfce1aca53fe370e0ba6b0191ea0f
zohaibkhan9870 <zaibprops@gmail.com>
2026-02-05 07:59:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -629,7 +629,7 @@ if run_button:
 
         all_probs_3 = []
 
-for i in range(START, temp3.shape[0], STEP):
+     for i in range(START, temp3.shape[0], STEP):
     train = temp3.iloc[:i]
     test = temp3.iloc[i:i + STEP]
 
